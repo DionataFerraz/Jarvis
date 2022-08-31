d055429e787136bf1ef366e3d5404cc62d867fc5
DionataFerraz <diouferraz@gmail.com>
2022-08-31 12:00:48 -0300

    feat: Refactoring theme

--- app/src/main/java/br/com/dionataferraz/jarvis/MainActivity.kt
+++ app/src/main/java/br/com/dionataferraz/jarvis/MainActivity.kt
@@ -3,10 +3,13 @@ package br.com.dionataferraz.jarvis
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.dionataferraz.jarvis.ui.theme.JarvisTheme
 
@@ -15,8 +18,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JarvisTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
--- app/src/main/java/br/com/dionataferraz/jarvis/ui/theme/Color.kt
+++ app/src/main/java/br/com/dionataferraz/jarvis/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package br.com.dionataferraz.jarvis.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple100 = Color(0xFFA022D2)
+val Purple400 = Color(0xFF4C1064)
+val Orange100 = Color(0xFFFFBA49)
