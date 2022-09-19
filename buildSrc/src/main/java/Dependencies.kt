b9f2c1f0e1b7bf9177fcfdeb5774b11cff750e4c
Dionata <diouferraz@gmail.com>
2022-09-19 15:58:03 -0300

    feat: Creating buildSrc to lib version

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,14 @@
+object ApplicationId {
+    const val id = "br.com.jarvis"
+}
+
+object Versions {
+    const val compileSdk = 31
+    const val minSdk = 21
+    const val targetSdk = 31
+}
+
+object Releases {
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
